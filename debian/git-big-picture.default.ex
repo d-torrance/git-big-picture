f473c333deacf93364b40e7b7c335ab8b94d76dc
Doug Torrance <dtorrance@monmouthcollege.edu>
2015-01-06 22:56:31 -0600

    debian: Add directory; result of dh_make.

---
+++ debian/git-big-picture.default.ex
@@ -0,0 +1,10 @@
+# Defaults for git-big-picture initscript
+# sourced by /etc/init.d/git-big-picture
+# installed at /etc/default/git-big-picture by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(git-big-picture):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="git-big-picture" command="/usr/bin/git-big-picture"
