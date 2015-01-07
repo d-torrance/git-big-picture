ee2b08c412e93429be9be6056da7975257c76a21
Doug Torrance <dtorrance@monmouthcollege.edu>
2015-01-06 22:57:15 -0600

    debian/(*.ex|*.EX): Remove files.

--- debian/git-big-picture.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the git-big-picture package
-#
-0 4	* * *	root	[ -x /usr/bin/git-big-picture_maintenance ] && /usr/bin/git-big-picture_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(git-big-picture):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="git-big-picture" command="/usr/bin/git-big-picture"
